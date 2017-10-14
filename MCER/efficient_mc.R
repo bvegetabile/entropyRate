6494711e0b57a7270a3c4c2b66b7e0b5114bacc9
Brian Vegetabile <bvegetabile@gmail.com>
2017-10-13 17:09:23 -0700

    new code with c++

---
+++ MCER/efficient_mc.R
@@ -0,0 +1,109 @@
+source('~/git/entropyRate/entropyRate.R')
+
+tm <- matrix(c(0.2,0.8,0.9,0.1),2,2,T)
+CalcEigenStationary(tm)
+mc <- SimulateMarkovChain(tm, 1000)
+CalcEmpiricalStationary(mc, 1:2)
+
+
+efficient_mc_er <- function(mc_seq, mc_order=1){
+  unique_states <- as.character(unique(mc_seq))
+  n_states <- length(unique_states)
+  state_space <- data.frame(matrix(NA, nrow=n_states, ncol=mc_order))
+  for(i in 1:mc_order){
+    state_space[,i] <- unique_states
+  }
+  state_space <- c(expand.grid(state_space), sep="")
+  state_space <- do.call(paste, state_space)
+  char_seq <- paste(mc_seq, collapse = '')
+  # print(unique_states)
+  mcer(char_seq, mc_order, unique_states, state_space)
+}
+
+efficient_lz77 <- function(mc_seq, window_size = 10){
+  char_seq <- paste(mc_seq, collapse = '')
+  lz77entropy(char_seq, window_size)
+}
+
+
+for(o in 1:10){
+  print(efficient_mc_er(mc, mc_order=o))
+}
+
+n_sims <- 1000
+mc_lens <- c(100,250,500,1000,2500)
+entropy_results <- matrix(NA, nrow = n_sims, ncol = length(mc_lens))
+for(j in 1:length(mc_lens)){
+  for(i in 1:n_sims){
+    mc <- SimulateMarkovChain(tm, mc_lens[j])
+    entropy_results[i,j] <- efficient_mc_er(mc, 2)
+  }
+  message('.', appendLF = F)
+}
+plot(mc_lens, sqrt(apply(entropy_results, 2, var)))
+CalcMarkovEntropyRate(tm, CalcEigenStationary(tm))
+boxplot.matrix(entropy_results, ylim=c(0,1))
+abline(h=CalcMarkovEntropyRate(tm, CalcEigenStationary(tm)), lty=3, col='red')
+
+block_bootstrap <- function(mc_seq, block_size){
+  n_blocks <- floor(length(mc_seq) / block_size)
+  new_seq <- rep(NA, length = n_blocks * block_size)
+  blocks <- sample(1:n_blocks, replace = T)
+  for(i in 1:n_blocks){
+    beg_ind <- (i-1)*block_size + 1
+    end_ind <- beg_ind + block_size - 1
+
+    block_start <- (blocks[i] - 1) * block_size + 1
+    block_end <- block_start + block_size - 1
+    new_seq[beg_ind:end_ind] <- mc_seq[block_start:block_end]
+  }
+  return(new_seq)
+}
+
+set.seed(1278)
+mc <- SimulateMarkovChain(tm, 1000)
+bs_samps <- 100
+block_sizes <- seq(10, 100, 5)
+bs_results <- matrix(NA, nrow=bs_samps, ncol=length(block_sizes))
+bs_results2 <- matrix(NA, nrow=bs_samps, ncol=length(block_sizes))
+for(bs in 1:length(block_sizes)){
+  for(B in 1:bs_samps){
+    ns <- block_bootstrap(mc_seq = mc, block_size = block_sizes[bs])
+    bs_results[B, bs] <- efficient_mc_er(ns, mc_order=1)
+    bs_results2[B, bs] <- SWLZEntRate(ns)
+  }
+}
+print()
+plot(block_sizes, apply(bs_results, 2, sd), type='l', ylim=c(0,0.1))
+lines(block_sizes, apply(bs_results2, 2, sd), type='l', col='blue')
+abline(h=apply(entropy_results,2,sd)[4])
+
+for(i in 1:500){
+  uh <- efficient_lz77(mc, i)
+  print(c(uh, CalcMarkovEntropyRate(tm, CalcEigenStationary(tm))))
+}
+
+
+mc <- SimulateMarkovChain(tm, 500)
+win_sizes <- seq(10,250, 10)
+win_res <- matrix(NA, nrow=length(win_sizes), ncol=2)
+for(i in 1:length(win_sizes)){
+  win_res[i,] <- c(win_sizes[i],
+                   efficient_lz77(mc, win_sizes[i]))
+}
+plot(win_res[,1], win_res[,2])
+abline(h=CalcMarkovEntropyRate(tm, CalcEigenStationary(tm)), lty=3, col='red')
+abline(h=SWLZEntRate(mc))
+
+n_sims <- 1000
+sim_res <- matrix(NA, nrow=n_sims, ncol=3)
+for(s in 1:n_sims){
+  mc <- SimulateMarkovChain(tm, 250)
+  sim_res[s, ] <- c(efficient_mc_er(mc),
+                    efficient_lz77(mc, 125),
+                    SWLZEntRate(mc))
+}
+boxplot.matrix(sim_res)
+abline(h=CalcMarkovEntropyRate(tm, CalcEigenStationary(tm)), lty=3, col='red')
+
+pairs(sim_res)
---
+++ MCER/mcer.cpp
@@ -0,0 +1,111 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+# include <RcppArmadillo.h>
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+int find_pos(CharacterVector v, std::string s){
+  CharacterVector::iterator it;
+  int pos = 0;
+  for(it = v.begin(); it < v.end(); it++){
+    if(*it == s){
+      return pos;
+    } else {
+      pos += 1;
+    }
+  }
+  return 0;
+}
+
+// [[Rcpp::export]]
+double mcer(std::string mc_seq,
+               int mc_order,
+               CharacterVector uniq_states,
+               CharacterVector state_space) {
+  arma::mat tc(state_space.length(),
+               uniq_states.length(),
+               arma::fill::zeros);
+  arma::mat tm(state_space.length(),
+               uniq_states.length(),
+               arma::fill::zeros);
+  arma::vec sm(state_space.length(),
+               arma::fill::zeros);
+
+  int mc_len = mc_seq.length();
+  std::string x_minus;
+  std::string x_plus;
+  int r_pos;
+  int c_pos;
+
+  for(int i = 0; i < mc_len - mc_order; i++){
+    x_minus = mc_seq.substr(i, mc_order);
+    x_plus = mc_seq.substr(i+mc_order, 1);
+    r_pos = find_pos(state_space, x_minus);
+    c_pos = find_pos(uniq_states, x_plus);
+    tc(r_pos, c_pos) += 1;
+    sm(r_pos) += 1;
+  }
+
+  sm = sm / arma::accu(sm);
+
+  arma::mat rs(state_space.length(),
+               uniq_states.length(),
+               arma::fill::zeros);
+  arma::mat sm_mat(state_space.length(),
+                   uniq_states.length(),
+                   arma::fill::zeros);
+
+  rs.each_col() = arma::sum(tc, 1);
+  sm_mat.each_col() = sm;
+
+  tm = tc / rs;
+
+  arma::mat res = - tm % log2(tm) % sm_mat;
+
+  res.replace(arma::datum::nan, 0);
+
+  return arma::accu(res);
+}
+
+
+// [[Rcpp::export]]
+double lz77entropy(std::string x, int window_size = 10){
+  int str_len = x.length();
+  int start_pos = window_size;
+  int right_pointer = start_pos;
+  int pos = start_pos;
+  int max_pointer = str_len - 1;
+  int n_char;
+  int counter = 0;
+  double L = 0;
+  double ent = 0;
+
+  std::string window;
+  std::string right_word;
+
+  while(right_pointer < max_pointer){
+    window = x.substr(pos-window_size, window_size);
+    while(right_pointer <= max_pointer){
+      n_char = right_pointer - pos + 1;
+      right_word = x.substr(pos, n_char);
+      if(window.find(right_word) == -1){
+        counter += 1;
+        L += n_char;
+        ent += std::log2(window_size) / n_char;
+        pos += 1;
+        right_pointer = pos;
+        // std::cout << right_word << '\n';
+        break;
+      } else {
+        right_pointer += 1;
+      }
+    }
+  }
+
+
+
+
+  return ent / counter;
+}
+
+
